f323afbf357807b77b284433b3f5803caea9c560
Tanapon <tanapons273@gmail.com>
2019-07-05 23:19:11 +0700

    Delete Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -89,8 +89,10 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        Session()->flash('success','ลบข้อมูลเรียบร้อยแล้ว');
+        return redirect(route('categories.index'));
     }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -8,9 +8,11 @@
 			Category
 		</div>
 		<div class="card-body">
-			<table class="table">
+			@if($categories->count()>0)
+				<table class="table">
 				<thead>
 					<th>Name</th>
+					<th></th>
 					<th></th>
 				</thead>
 				<tbody>
@@ -20,11 +22,33 @@
 							<td>
 								<a href="{{route('categories.edit',$category->id)}}" class="btn btn-info btn-sm">Edit</a>
 							</td>
+							<td>
+								<form class="delete_form" action="{{route('categories.destroy',$category->id)}}" method="post">
+									@csrf
+									<input type="hidden" name="_method" value="DELETE">
+									<input type="submit" name="" value="Delete" class="btn btn-danger btn-sm">	
+								</form>
+							</td>
 						</tr>
 
 					@endforeach
 				</tbody>
 			</table>
+			@else
+				<h3 class="text text-center">No Category</h3>
+			@endif
 		</div>
 	</div>
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.delete_form').on('submit',function(){
+				if(confirm("คุณต้องการลบข้อมูลหรือไม่")){
+					return true;
+				}else{
+					return false;
+				}
+			});
+		});
+	</script>
 @endsection
